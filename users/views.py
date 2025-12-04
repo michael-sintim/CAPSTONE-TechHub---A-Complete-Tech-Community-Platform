e467d22d77fd17cf6b9ebdcac226ef7ebf6db158
Michael Sintim <michaelsintim673@gmail.com>
2025-12-04 10:25:23 +0000

    Updates

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from .tokens import account_token_generator
 from .models import User
-from django.utils.encoding import force_bytes
+from django.utils.encoding import force_bytes,force_str
 from django.utils.http  import urlsafe_base64_encode,urlsafe_base64_decode
 from django.core.mail import EmailMessage
 from django.contrib.auth import login as auth_login, get_user_model
@@ -55,5 +55,22 @@ def login_view(request):
     return render(request,'login.html',{'form':form})
 
 def activate(request,uidb64,token):
-     User = get_user_model()
+    User = get_user_model()
     
+    try :
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError,ValueError,OverflowError,User.DoesNotExist):
+         user = None 
+
+    if user is not None and account_token_generator.check_token(user,token):
+         user.is_active = True
+         user.save()
+
+         messages.success(request,"Thank you for confirming your email")
+         return redirect ('login')
+
+    else:
+         messages.error(request,"Activation link is expired")
+         form = Registration()
+         return render(request, 'register.html',{'form':form})
