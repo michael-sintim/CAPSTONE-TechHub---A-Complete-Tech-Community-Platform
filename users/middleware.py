334a391396236f2ba45479509666e152fb02acf5
Michael Sintim <michaelsintim673@gmail.com>
2025-12-06 09:43:50 +0000

    updates

--- users/middleware.py
+++ users/middleware.py
@@ -1,24 +1,7 @@
 from django.utils import timezone
+import logging
+from django.utils import timezone
+from django.urls import resolve
+from django.db.models import F
+from resources.models import Resource
 
-class LoggingMiddleware:
-    def __init__(self,get_response):
-        self.get_response = get_response
-
-    def __call__(self,request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-        if request.user.is_authenticated:
-
-            current_time = timezone.now()
-            response = self.get_response(request)
-        
-        # Code to be executed for each request/response after
-        # the view is called.
-        return response
-    
-
-
-def square_numbers(a,b):
-     a **  b
-
-print(square_numbers(3,2))
