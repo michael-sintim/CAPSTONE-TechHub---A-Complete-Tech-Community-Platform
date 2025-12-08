9b89def9629968dd10bf14021513acf04b5f3867
Michael Sintim <michaelsintim673@gmail.com>
2025-12-08 18:25:52 +0000

    Updates

--- bugs/admin.py
+++ bugs/admin.py
@@ -4,13 +4,27 @@ from .models import Bug,BugHistory
 
 @admin.register(Bug)
 class BugAdmin(admin.ModelAdmin):
-    list_display = ('title','status','priority','bug','changed_by','timestamp')
-    list_filter = ('status','priority')
+    list_display = ('title','status','priority','project','reporter')
+    list_filter = ('status','priority','project__title')
     date_hierarchy = 'created_at'
-    list_per_page = 10
+    list_per_page = 20
+
+    fieldsets = (
+        ("Bug Details",{
+            'fields': ('title','description','file_attachment')
+        }),
+
+        ("Status",{
+            'fields': ('status','priority','project')
+        }),
+        (
+            'People',{'fields':('reporter','assigned_to')}
+        ))
 
 @admin.register(BugHistory)
 class BugHistoryAdmin(admin.ModelAdmin):
-    list_display = ('old_status','new_status')
+    list_display = ('bug','changed_by','timestamp')
     list_filter = ('old_status','new_status')
     search_fields = ('old_status','new_status')
+    readonly_fields = ('bug', 'old_status', 'new_status', 'changed_by', 'timestamp')
+
