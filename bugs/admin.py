fe675cb9295a6771d0bea819e15b0e50876dfecc
Michael Sintim <michaelsintim673@gmail.com>
2025-12-08 16:27:05 +0000

    Updates

--- bugs/admin.py
+++ bugs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Bug,BugHistory
 # Register your models here.
+
+@admin.register(Bug)
+class BugAdmin(admin.ModelAdmin):
+    list_display = ('title','status','priority','bug','changed_by','timestamp')
+    list_filter = ('status','priority')
+    date_hierarchy = 'created_at'
+    list_per_page = 10
+
+@admin.register(BugHistory)
+class BugHistoryAdmin(admin.ModelAdmin):
+    list_display = ('old_status','new_status')
+    list_filter = ('old_status','new_status')
+    search_fields = ('old_status','new_status')
