701c29ebe6744d68218e1a5bae16e1828c33b234
Michael Sintim <michaelsintim673@gmail.com>
2025-12-08 16:15:26 +0000

    admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Project,ProjectImage
 # Register your models here.
+
+
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1 
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title','status','user','category','is_public','due_date',)
+    list_filter = ('status','category','user__username')
+    search_fields = ("title","description",'status')
+    readonly_fields= ('slug','created_at','updated_at')
+    date_hierarchy = 'created_at'
+    list_per_page = 20 
+    inlines = [ProjectImageInline]
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_username','get_email','urls','bio')
+    list_filter  = ('is_active',)
+    search_fields = ('bio',)
+    date_hierarchy = 'created_at'
+
+    def get_username(self,obj):
+        return obj.user.username
+    get_username.short_desciption = "Username"
+    
+    def get_email(self,obj):
+        return obj.user.email
+    get_email.short_description = 'Email '
