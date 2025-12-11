4d0ce85aec6e09ade26798e2a272e117dbbcbce7
Michael Sintim <michaelsintim673@gmail.com>
2025-12-11 15:09:34 +0000

    Updates

--- users/models.py
+++ users/models.py
@@ -24,13 +24,11 @@ class Post(TimeStampModel):
     def __str__(self):
         return self.title
 
-class Profile(models.Model):
+class Profile(TimeStampModel):
     id = models.UUIDField(default=uuid.uuid4,editable=False, primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'user_profile')
     skills = models.ManyToManyField(Skill, related_name="skills_profile")
     following = models.ManyToManyField('self',symmetrical=False,blank=True, related_name='following_profile')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
     last_activity = models.DateTimeField(auto_now=True)
     urls = models.URLField(max_length=300,blank=True,null=True)
@@ -47,12 +45,11 @@ class Profile(models.Model):
         permissions = [
             ('can_verify_profile','Can verify user profile')
         ]
-        db_table = 'Profile'
+        db_table = 'profile'
         ordering = ['-created_at']
         verbose_name = "Profile"
         verbose_name_plural = "Profiles"
         indexes = [
-            models.Index(fields=['user']),
             models.Index(fields=['is_active','-created_at']),
             
         ]
